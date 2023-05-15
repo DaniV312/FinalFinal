5c8dc8da902ae2a68be2ebeeed6e9768df130c36
DaniV312 <132933131+DaniV312@users.noreply.github.com>
2023-05-15 13:56:17 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -29,8 +29,6 @@ st.set_page_config(page_title = PAGE_TITLE, page_icon = PAGE_ICON)
 
 # load css, PDF #
 
-with open(css_file) as f:
-    st.markdown("<style>{}</style".format(f.read()), unsafe_allow_html = True)
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 with open(portfolio_file,"rb") as pdf_file:
